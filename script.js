d5adf80c79e8dc6aa4e03f3151e2b5579ff2fe37
RahmanFahim <nut02ral@gmail.com>
2026-07-03 22:11:13 +0600

    script completed

---
+++ script.js
@@ -0,0 +1,6 @@
+function welcomeMessage() {
+    alert("Welcome to TechStore! Check out our latest deals on the Products page.");
+}
+function submitForm() {
+    alert("Thank you for your message! Our team will get back to you within 24 hours.");
+}
